08599381ff245cd574060cd9c7c9bcfead01d5bb
Alexander Kulinich <necrysgd@gmail.com>
2019-01-05 11:19:42 +0300

    sending HTTP req to bot

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+  "fmt"
   "log"
+  "net/http"
   "os"
   "os/signal"
   "syscall"
@@ -10,6 +12,9 @@ import (
 )
 
 func main() {
+  // TODO: make configurable
+  botHost := "0.0.0.0:8080"
+
   isWorking := true
   sigs := make( chan os.Signal, 1 )
   go func() {
@@ -38,8 +43,17 @@ func main() {
       log.Fatal( err )
     }
 
-    // TODO: send to TG bot
-    log.Printf( "temperature: %.2f C, humidity: %.2f RH, pressure: %.2f mmHg\n", temperature, humidity, pressure )
+    url := fmt.Sprintf( "%s%s%st=%f&h=%f&p=%f", "http://", botHost, "/bme280/?", temperature, humidity, pressure )
+    resp, err := http.Get( url )
+    if err != nil {
+      log.Printf( "Error sending request: %v", err )
+      // Write data for debug purposes
+      log.Printf( "temperature: %.2f C, humidity: %.2f RH, pressure: %.2f mmHg\n", temperature, humidity, pressure )
+    } else {
+      log.Printf( "Response Status: %v", resp.Status )
+    }
+
+    // TODO: add local CSV log
 
     time.Sleep( logPeriod )
   }
